c93214a48f142e7f86c4653fb32f8b7978201987
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-08-21 19:19:01 +0530

    1st commit

---
+++ Components/Navbar.tsx
@@ -0,0 +1,48 @@
+// GLOBAL
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+
+
+// ICONS
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
+
+// LOCAL
+import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from "../utils";
+
+const Navbar = () => {
+const user =false;
+
+
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+			<Image 
+				className="cursor-pointer"
+				src={Logo}
+				alt="Tiktik"
+				layout="responsive"
+			/>
+		</div>
+      </Link>
+
+	  <div>Search</div>
+	  <div>
+		{user ? (
+			<div>Logged In</div>
+		) : <GoogleLogin
+			onSuccess={(response) => createOrGetUser(response)}
+			onError={() => console.log('Error')}
+		/>}
+	  </div>
+    </div>
+  );
+};
+
+export default Navbar;
